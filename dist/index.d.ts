03fa58761d8caddfb7d3185ceed960f532eddd07
AdrianRicoy <noe.ricoy@hotmail.com>
2025-05-30 22:23:30 -0600

    faat: implement config build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,11 @@
+import * as axios from 'axios';
+import { AxiosRequestConfig } from 'axios';
 
-export {  }
+declare const httpClient: {
+    get: <T>(baseUrl: string, path?: string, config?: AxiosRequestConfig) => Promise<axios.AxiosResponse<T, any>>;
+    post: <T>(baseUrl: string, path: string | undefined, data: any, config?: AxiosRequestConfig) => Promise<axios.AxiosResponse<T, any>>;
+    put: <T>(baseUrl: string, path: string | undefined, data: any, config?: AxiosRequestConfig) => Promise<axios.AxiosResponse<T, any>>;
+    delete: <T>(baseUrl: string, path?: string, config?: AxiosRequestConfig) => Promise<axios.AxiosResponse<T, any>>;
+};
+
+export { httpClient };
